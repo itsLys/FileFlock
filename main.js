c8218af0a89fd1e61f64eee848352683755971d4
Lys <elyesseen@gmail.com>
2024-04-06 14:05:52 +0000

    Refactor construtor into class

--- main.js
+++ main.js
@@ -7,22 +7,36 @@ function idGenerator() {
 const generateId = idGenerator();
 const library = [];
 const bookStatus = ["TO READ", "READING", "COMPLETED"];
-const imagePlaceholder =
+const coverPlaceholder =
   "https://www.marytribble.com/wp-content/uploads/2020/12/book-cover-placeholder.png";
-function Book(name, author, status = bookStatus[0], cover = imagePlaceholder) {
-  this.name = name;
-  this.author = author;
-  this.status = status;
-  this.ID = generateId();
-  this.cover = cover;
+class Book {
+  constructor(name, author, status = bookStatus[0], cover = coverPlaceholder) {
+    this.name = name;
+    this.author = author;
+    this.status = status;
+    this.ID = generateId();
+    this.cover = cover;
+  }
+  changeStatus(newStatus) {
+    // this.status = newStatus;
+    for (const status of bookStatus) {
+      if (status.toUpperCase == newStatus) {
+        this.status = status;
+      } // for each status of bookStatus, if newStatus is equal to status, this status is status
+      //
+    }
+  }
+  changeCover(coverLink) {
+    this.cover = coverLink;
+  }
 }
 
-function addBook(name, author, status) {
+function addBook(name, author, status, cover) {
   const newBookStatus = bookStatus.indexOf(status.toUpperCase());
   if (newBookStatus == -1) {
     return "please enter a valid status";
   } else {
-    const newBook = new Book(name, author, bookStatus[newBookStatus]);
+    const newBook = new Book(name, author, bookStatus[newBookStatus], cover);
     library.push(newBook);
   }
 }
@@ -36,21 +50,21 @@ function deleteBook(bookId) {
   }
 }
 
-function changeCover(bookId, coverLink) {
-  for (const book of library) {
-    if (book.ID == bookId) {
-      book.cover = coverLink;
-    }
-  }
-}
+// function changeCover(bookId, coverLink) {
+//   for (const book of library) {
+//     if (book.ID == bookId) {
+//       book.cover = coverLink;
+//     }
+//   }
+// }
 
-function changeStatus(bookId, newStatus) {
-  for (const book of library) {
-    if (book.ID == bookId) {
-      book.status = newStatus;
-    }
-  }
-}
+// function changeStatus(bookId, newStatus) {
+//   for (const book of library) {
+//     if (book.ID == bookId) {
+//       book.status = newStatus;
+//     }
+//   }
+// }
 
 addBook("The Great Gatsby", "F. Scott Fitzgerald", "Reading");
 addBook("To Kill a Mockingbird", "Harper Lee", "Completed");
